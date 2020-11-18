6de9eb9ad75769d83cbd22155bddc28f1b53064c
Adrian <adrian.barriento@juanxxiii.net>
2020-11-18 20:39:19 +0100

    arreglo

---
+++ app/src/main/java/com/example/practicamoviles_1tr/models/Location.java
@@ -0,0 +1,9 @@
+package com.example.practicamoviles_1tr.models;
+
+import lombok.Data;
+
+@Data
+public class Location {
+    private int latitude;
+    private int longitude;
+}
---
+++ app/src/main/java/com/example/practicamoviles_1tr/models/Pools.java
@@ -0,0 +1,6 @@
+package com.example.practicamoviles_1tr.models;
+
+public class Pools {
+    private String title;
+    private Location location;
+}
